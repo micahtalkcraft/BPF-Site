3ea2a85d94dc920028ec8b4f57af598af270a6bb
micahtalkcraft <micah@talkcraftai.com>
2026-02-25 15:07:50 +1100

    Convert to static build
    Convert to static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.bestpropfirms.com',
+  output: 'static',
   integrations: [react(), sitemap()],
 
   vite: {
